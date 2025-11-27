41900705a8e3e08636ae7200fa95de42db7f41d9
s1120394-creator <s1120394@dmhs.kh.edu.tw>
2025-11-27 14:46:38 +0800

    HW9

--- test-cpp/main.cpp
+++ test-cpp/main.cpp
@@ -1,22 +1,44 @@
 #include <iostream>
-#include <string>
+#include <cstdlib>   // rand(), srand()
+#include <ctime>     // time()
+
 using namespace std;
 
 int main() {
-    int score[10] = {85, 90, 60, 92, 100, 76, 50, 89, 84, 40};
-    int gradeCount[5] = {0};
-    for (int i = 0; i < 10; ++i) {
-        if (score[i] >= 90) {
-            gradeCount[0]++;
-        } else if (score[i] >= 80) {
-            gradeCount[1]++;
-        } else if (score[i] >= 70) {
-            gradeCount[2]++;
-        } else if (score[i] >= 60) {
-            gradeCount[3]++;
-        } else {
-            gradeCount[4]++;
+    const int N = 10;      // 陣列大小
+    int arr[N];
+
+    //-----亂數種子-----
+    srand(time(NULL));
+
+    //-----產生 0~99 之間的亂數-----
+    cout << "原始亂數陣列: ";
+    for (int i = 0; i < N; i++) {
+        arr[i] = rand() % 100;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    //-----選擇排序 (Selection Sort)-----
+    for (int i = 0; i < N - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < N; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
         }
+        // 交換
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
 
-    cout << "A等人數:
+    //-----排序後輸出-----
+    cout << "排序後陣列: ";
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
